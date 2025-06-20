6f248d3d611beffe67d2de5e69e294dd53b81850
Lorenzo Orlando <orlandolorenzomk@gmail.com>
2025-06-20 21:33:57 +0300

    update migration

--- src/main/resources/db/migration/V15__create_email_configuration_table.sql
+++ src/main/resources/db/migration/V15__create_email_configuration_table.sql
@@ -1,10 +1,18 @@
 CREATE TABLE email_configuration (
-    id UUID PRIMARY KEY  DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
-    security_protocol VARCHAR(20) NOT NULL CHECK (security_protocol IN ('NONE', 'SSL', 'TLS', 'STARTTLS')),
-    description TEXT,
-    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP WITHOUT TIME ZONE,
-    use_debug BOOLEAN NOT NULL,
-    email_provider VARCHAR(20) NOT NULL CHECK (email_provider IN ('SMTP', 'MAILJET'))
+     id UUID PRIMARY KEY DEFAULT (
+         (
+             lpad(trunc(random()*1e8)::text, 8, '0') || '-' ||
+             lpad(trunc(random()*1e4)::text, 4, '0') || '-' ||
+             lpad(trunc(random()*1e4)::text, 4, '0') || '-' ||
+             lpad(trunc(random()*1e4)::text, 4, '0') || '-' ||
+             lpad(trunc(random()*1e12)::text, 12, '0')
+             )::uuid
+         ),
+     name VARCHAR(255) NOT NULL,
+     security_protocol VARCHAR(20) NOT NULL CHECK (security_protocol IN ('NONE', 'SSL', 'TLS', 'STARTTLS')),
+     description TEXT,
+     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+     updated_at TIMESTAMP WITHOUT TIME ZONE,
+     use_debug BOOLEAN NOT NULL,
+     email_provider VARCHAR(20) NOT NULL CHECK (email_provider IN ('SMTP', 'MAILJET'))
 );
