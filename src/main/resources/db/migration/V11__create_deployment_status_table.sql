9ee23bdcb0350d2a6f2725553b9fdb0dbb1ba4ca
Lorenzo Orlando <orlandolorenzomk@gmail.com>
2025-06-20 21:31:07 +0300

    update migration

--- src/main/resources/db/migration/V11__create_deployment_status_table.sql
+++ src/main/resources/db/migration/V11__create_deployment_status_table.sql
@@ -1,16 +1,16 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Needed for UUID generation
-
 CREATE TABLE deployment_status (
-   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-   deployment_id SERIAL NOT NULL,
+   id UUID PRIMARY KEY DEFAULT (
+       (
+           lpad(trunc(random()*1e8)::text, 8, '0') || '-' ||
+           lpad(trunc(random()*1e4)::text, 4, '0') || '-' ||
+           lpad(trunc(random()*1e4)::text, 4, '0') || '-' ||
+           lpad(trunc(random()*1e4)::text, 4, '0') || '-' ||
+           lpad(trunc(random()*1e12)::text, 12, '0')
+           )::uuid
+       ),
    status VARCHAR(20) NOT NULL CHECK (status IN ('SUCCESS', 'FAILURE', 'NOT_RUN')),
    message TEXT,
    type VARCHAR(20) NOT NULL CHECK (type IN ('UPDATE', 'BUILD', 'RUN')),
    logs_path TEXT,
-   created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
-
-   CONSTRAINT fk_deployment
-       FOREIGN KEY (deployment_id)
-           REFERENCES deployments (id)
-           ON DELETE CASCADE
+   created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
 );
